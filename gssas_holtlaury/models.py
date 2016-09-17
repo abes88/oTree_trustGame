e240a9ba6d01a4ce41d4be404315a8cf5725c6ae
Juan B Cabral <jbc.develop@gmail.com>
2016-09-17 20:05:38 -0300

    view

--- gssas_holtlaury/models.py
+++ gssas_holtlaury/models.py
@@ -28,14 +28,14 @@ class Group(BaseGroup):
 class Player(BasePlayer):
 
     # GSS
-    people_try_to_take_advantage_of_you_if_they_got_a_chance = models.Charfield(
+    people_try_to_take_advantage_of_you_if_they_got_a_chance = models.CharField(
         max_length=100, choices=('Would take advantage of you', 'Would try to be fair'),
         verbose_name="Do you think most people would try to take advantage of you if they got a chance, or would they try to be fair?")
 
-    most_of_the_time_people_try_to_be_helpful = models.Charfield(
+    most_of_the_time_people_try_to_be_helpful = models.CharField(
         max_length=100, choices=('Try to be helpful', 'Just look out for themselves'),
         verbose_name="Would you say that most of the time people try to be helpful, or that they are mostly just looking out for themselves?")
 
-    that_most_people_can_be_trusted = models.Charfield(
+    that_most_people_can_be_trusted = models.CharField(
         max_length=100, choices=('Most people can be trusted', "Can't be too careful"),
         verbose_name="Generally speaking, would you say that most people can be trusted or that you can't be too careful in dealing with people?")
--- gssas_holtlaury/views.py
+++ gssas_holtlaury/views.py
@@ -5,7 +5,13 @@ from .models import Constants
 
 
 class GSSAS(Page):
-    pass
+
+    form_model = models.Player
+    form_fields = [
+        "people_try_to_take_advantage_of_you_if_they_got_a_chance",
+        "most_of_the_time_people_try_to_be_helpful",
+        "that_most_people_can_be_trusted "
+    ]
 
 
 page_sequence = [
